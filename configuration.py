24665d55f37e1610655785e393a69ddfe5a22558
Evelyn Velazquez <velazquezperazzoevelyn@gmail.com>
2024-11-04 21:29:29 -0300

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-2959b6f9-7630-428b-9d6b-595328081081.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
